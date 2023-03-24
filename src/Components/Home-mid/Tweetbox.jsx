66fde4c25d48cb5bbbff5f7317601ffd44333851
neha <nehapurshottamchaudhary@gmail.com>
2023-03-24 19:22:14 +0530

    first commit

---
+++ src/Components/Home-mid/Tweetbox.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./Tweetbox.css";
+// import ape from '../ape.jpg'
+import {Avatar,Button} from 'antd'
+
+export function Tweetbox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    const posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+    const newPost = {
+      displayName: "Neha Chaudhary",
+      username: "Vaatsalya",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://c8.alamy.com/zooms/9/f812ac99952345a885520b02b8007c25/2hcgdan.jpg",
+    };
+
+    localStorage.setItem("posts", JSON.stringify([newPost, ...posts]));
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar size='large' src={'https://c8.alamy.com/zooms/9/f812ac99952345a885520b02b8007c25/2hcgdan.jpg'} />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+        <Button 
+          onClick={sendTweet}
+          type="primary"
+          className="tweetBox__tweetButton"
+        >
+        Tweet    
+        </Button>
+      </form>
+    </div>
+  );
+}
+
